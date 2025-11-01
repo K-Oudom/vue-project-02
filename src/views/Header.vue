674d57d2e22e729694ca58632b2273633e69623d
dmk <motedee02@gmail.com>
2025-11-01 23:59:52 +0700

    Updated

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,21 +1,87 @@
 <template>
-    <div>
-        <ul>
-            <li>
-                <router-link :to="{name:'home'}" class="active" id="home"><i class="bi bi-house navicon"></i>Home</router-link>
-            </li>
-            <li>
-                <router-link :to="{name:'about'}" id="about"><i class="bi bi-person navicon"></i>About</router-link>
-            </li>
-            <li>
-                <router-link :to="{name:'resume'}" id="resume"><i class="bi bi-file-earmark-text navicon"></i>Resume</router-link>
-            </li>
-            <li>
-                <router-link :to="{name:'skill'}" id="skill"><i class="bi bi-briefcase navicon"></i>Skill</router-link>
-            </li>
-            <li>
-                <router-link :to="{name:'contact'}" id="contact"><i class="bi bi-envelope navicon"></i>Contact</router-link>
-            </li>
-      </ul>
+  <header id="header" :class="`header dark-background d-flex flex-column ${menuActive}`">
+    <i :class="`header-toggle d-xl-none ${menuIcon}`" @click="toggleMenu"></i>
+    <!-- bi-x-lg -->
+    <div class="profile-img">
+      <img src="../assets/img/profile/me.png" alt="" class="img-fluid rounded-circle">
     </div>
+
+    <router-link :to="{name:'home'}" class="logo d-flex align-items-center justify-content-center">
+         <h1 class="sitename" v-if="data">{{ data.name === '' ? 'Alex Smit' : data.name }}</h1>
+    </router-link>
+
+    <div class="social-links text-center">
+      <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
+      <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+      <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+      <a href="#" class="google-plus"><i class="bi bi-telegram"></i></a>
+      <a href="#" class="linkedin"><i class="bi bi-whatsapp"></i></a>
+    </div>
+
+    <nav id="navmenu" class="navmenu">
+      <ul>
+        <li>
+            <router-link :to="{name:'home'}" class="active" id="home"><i class="bi bi-house navicon"></i>Home</router-link>
+        </li>
+        <li>
+            <router-link :to="{name:'about'}" id="about"><i class="bi bi-person navicon"></i>About</router-link>
+        </li>
+        <li>
+            <router-link :to="{name:'resume'}" id="resume"><i class="bi bi-file-earmark-text navicon"></i>Resume</router-link>
+        </li>
+        <li>
+            <router-link :to="{name:'skill'}" id="skill"><i class="bi bi-briefcase navicon"></i>Skill</router-link>
+        </li>
+        <li>
+            <router-link :to="{name:'contact'}" id="contact"><i class="bi bi-envelope navicon"></i>Contact</router-link>
+        </li>
+      </ul>
+    </nav>
+
+  </header>
 </template>
+<script>
+//   import('@/assets/js/link');
+  
+  export default {
+    data(){
+      return {
+        data:null,
+        error:null,
+        menuActive:null,
+        menuIcon:'bi bi-list'
+      }
+    },
+    mounted(){
+      fetch('http://localhost:3000/data/hero.json')
+        .then((res) => (res.json()))
+        .then((json) => (this.data = json))
+        .catch((err) => console.log(err.message));
+    },
+
+    methods:{
+      toggleMenu(){
+        if(this.menuActive === null){
+          this.menuActive = 'active';
+          this.menuIcon = 'bi bi-x-lg';
+        }
+        else{
+          this.menuActive = null;
+          this.menuIcon = 'bi bi-list';
+        }
+      }
+    }
+    
+  }
+</script>
+<style scoped>
+  ul li a{
+    text-decoration:none;
+  }
+  ul li a.active i{
+    color: #149ddd;
+  }
+  .header > a{
+    text-decoration: none;
+  }
+</style>
